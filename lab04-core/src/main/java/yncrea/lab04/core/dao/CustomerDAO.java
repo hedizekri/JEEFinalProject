d863588759ea54ff99dcd8596f5a04f9fab21b52
Hedi Zekri <hedi.zekri@isen.yncrea.fr>
2020-02-17 15:51:52 +0100

    Supression

--- lab04-core/src/main/java/yncrea/lab04/core/dao/CustomerDAO.java
+++
@@ -1,8 +0,0 @@
-package yncrea.lab04.core.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import yncrea.lab04.core.entity.Customer;
-
-public interface CustomerDAO extends JpaRepository<Customer, Long> {
-
-}
--- lab04-core/src/main/java/yncrea/lab04/core/dao/ProjectDAO.java
+++
@@ -1,8 +0,0 @@
-package yncrea.lab04.core.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import yncrea.lab04.core.entity.Project;
-
-public interface ProjectDAO extends JpaRepository<Project,Long> {
-
-}
